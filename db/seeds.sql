bc78a67d241a17aa942e0838ceefeeaaa567baf8
JordonMoses <jordon@blackbirdrevolt.com>
2021-01-10 13:46:26 -0600

    seeds.sql completed

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Cauliflower's Cumin From Inside the House Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Poblano Picasso Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Human Polenta-pede Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('I Know Why the Cajun Burger Sings', false);
+
