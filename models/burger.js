5d388333230651ae65509665d9652b20dff9efe1
JordonMoses <jordon@blackbirdrevolt.com>
2021-01-10 14:29:47 -0600

    burger.js updated

---
+++ models/burger.js
@@ -0,0 +1,32 @@
+const orm = require("../config/orm.js");
+
+
+let burger = {
+
+    selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+
+  updateOne: function (objColVals, boolean, condition, cb) {
+    orm.updateOne("burgers", objColVals, boolean, condition, function (res) {
+      cb(res);
+    });
+  },
+
+  deleteOne: function (condition, cb) {
+    orm.deleteOne("burgers", condition, function (res) {
+      cb(res);
+    });
+  },
+};
+
+
+module.exports = burger
